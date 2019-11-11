39bb856e52e1bdfe322f262d8c9d2b0d56cc46fb
Reece Pierson <piersonreece@gmail.com>
2019-11-11 16:04:54 -0600

    Made MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path='/' component={Home} /> //path defines when the component is to be displayed
+    <Route path='/about' component={About} />
+    <Route path='/contact' component={Contact} />
   </div>
+  
+
 );
 
 export default App;
